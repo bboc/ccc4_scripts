91db16a953990e2bf05ae5193d6339421162a28d
bboc <bboc@gmx.net>
2015-02-03 12:08:58 +0100

    disable spotlight before indexing

---
+++ disable-spotlight-on-volume.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# disable-spotlight-on-volume.sh
+#
+# This script disables spotlight indexing for the destination volume, to avoid
+# the operation being slowed down by the spotlight indexer.
+#
+# This script should work with OSX 10.6 through at least 10.9
+# (use 'man mdutil' in the terminal to see if the parameters match on your version of OS-X)
+#
+# if you run into issues with spotlight (e.g. an error message that says Spotlight Server disabled)
+# you can stop and restart the service
+# stop spotlight completely:
+# sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+# start it again (this will update all indexes and may slow your system down)
+# sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+#
+# don't forget to chmod 755 this script
+
+source="$1"
+dest="$2"
+exitStatus=$3
+
+# disable spotlight search for the volume
+mdutil -d $dest
+# disable indexing for the volume
+mdutil -i $dest
+# erase local store for volume, might not be necessary but thrown in for good measure
+mdutil -E $dest
+# write a metadata never index to destination so it will not be re-indexed even if you cloned .Spotlight-V100
+touch {$dest}.metadata_never_index
